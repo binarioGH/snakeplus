965dd1682ade2de2f85cb3d8ef470f06bd5121bf
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-03-24 22:07:11 -0600

    mejoras vergas

--- snake.cpp
+++ snake.cpp
@@ -1,101 +1,137 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
+#include <list>
+using namespace std;
+
+#define W 119
+#define S 115
+#define A 97
+#define D 100
+
 void gotoxy(int x, int y);
-void hideCursor(void);
-void drawBorders(void);
+void hidecursor(void);
+void drawBorders(int bx, int by);
+
 class Food{
 private:
-	int x=0, y = 0;
+	int x,y;
 public:
-	Food(void);
+	Food(int xx, int yy){reload(xx,yy);}
+	void reload(int _x, int _y){
+		x = _x;
+		y = _y;
+		gotoxy(x,y);printf("*");
+	}
 	int X(void){return x;}
 	int Y(void){return y;}
-	void reload(void);
 };
-Food::Food(void){
-	Food::reload();
-}
-void Food::reload(){
-	gotoxy(x,y);printf(" ");
-	x = rand()%75+5;
-	y = rand()%17+5;
-	gotoxy(x,y);printf("*");
-}
-class Snake{
+
+class Body{
 private:
 	int x, y;
-	int xDirection=-1, yDirection=0;
-	void draw(void);
-	void collition(void);
+	int dx, dy;
 public:
-	Snake(int _x, int _y){x=_x;y=_y;}
-	void move(void);
+	Body(int _x, int _y){
+		x = _x;
+		y = _y;
+	}
+	~Body(void){}
 	int X(void){return x;}
 	int Y(void){return y;}
-	bool alive = true;
+	int xDest, yDest;
+	void move(void);
 };
-void Snake::move(void){
-	Snake::draw();
-	x+=xDirection;
-	y+=yDirection;
-	if(kbhit()){
-		char key = getch();
-		switch(key){
-			case 'w':xDirection = 0;yDirection = -1;break;
-			case 's':xDirection=0;yDirection = 1;break;
-			case 'a':xDirection=-1;yDirection =0;break;
-			case 'd':xDirection=1;yDirection=0;break;
-		}
+
+class Head{
+private:
+	int x, y;
+	int dx=-1, dy=0;
+	int face = 17;
+	void collition(void);
+	
+public:
+	Head(int _x, int _y){
+		x = _x;
+		y = _y;
 	}
-	Snake::collition();
-	return;
-}
-void Snake::draw(void){
+	~Head(void){}
+	void keyMove(void);
+	void move(int key);
+	bool death = false;
+	int X(void){return x;}
+	int Y(void){return y;}
+	list<Body*> tail;
+	list<Body*>::iterator tailIterator;
+};
+
+void Head::move(int key){
 	gotoxy(x,y);printf(" ");
-	gotoxy(x+xDirection,y+yDirection);printf("%c",223);
+	switch(key){
+		case W:dx=0;dy=-1;face=30;break;
+		case S:dx=0;dy=1;face=31;break;
+		case A:dx=-1;dy=0;face=17;break;
+		case D:dx=1;dy=0;face=16;break;
+		default:
+		    x += dx;
+		    y += dy; 
+		break;
+	}
+	Head::collition();
+	gotoxy(x,y);printf("%c",face);
 	return;
 }
-void Snake::collition(void){
-	if(x<=3 || x>= 77){
-		alive = false;
+void Head::keyMove(void){
+	char key = '?';
+	if(kbhit()){
+		key = getch();
 	}
-	if(y<=4 || y>= 23){
-		alive = false;
-	}
+	Head::move(key);
 	return;
 }
-int main(){
-	system("cls");
-	hideCursor();
-	drawBorders();
-	Snake s (40,11);
-	Food p;
-	int score = 00;
-	gotoxy(35, 2);printf("Puntos:  %i", score);
-	while(s.alive){
-		if(p.X() == s.X() && p.Y() == s.Y()){
-			score += 1;
-			gotoxy(35,2);printf("                   ");
-			gotoxy(35,2);printf("Puntos:  %i", score);
-			p.reload();
+
+void Head::collition(void){
+	if(x<=2 || x>=78 || y <= 2 || y >= 22){
+		death = true;
+		return;
+	}
+	for(tailIterator=tail.begin();tailIterator != tail.end();tailIterator++){
+		if((*tailIterator)->X() == x && (*tailIterator)->Y() == y){
+			death = true;
+			return;
 		}
-		s.move();
+	}
+	return;
+}
+
+int main(int nArgs, char* argv[]){
+	system("cls");
+	hidecursor();
+	drawBorders(219, 219);
+	Head snake (40,12);
+	Food points (rand()%75+4, rand()%17+4);
+	while(!snake.death){
+		snake.keyMove();
+		if((snake.X() == points.X()) && (snake.Y() == points.Y())){
+			points.reload(rand()%75+4, rand()%18+4);
+		}
 		Sleep(100);
 	}
+
 	return 0;
 }
 
 void gotoxy(int x, int y){
 	HANDLE hCon;
 	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD dwpos;
-	dwpos.X = x;
-	dwpos.Y = y;
-	SetConsoleCursorPosition(hCon, dwpos);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hCon, dwPos);
 	return;
 }
-void hideCursor(void){
+
+void hidecursor(void){
 	HANDLE hCon;
 	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO cci;
@@ -104,20 +140,20 @@ void hideCursor(void){
 	SetConsoleCursorInfo(hCon, &cci);
 	return;
 }
-void drawBorders(void){
-	int x,  y=3;
-	for(x=2;x<78;x++){
-		gotoxy(x,y);printf("%c",205);
-		gotoxy(x,y+20);printf("%c", 205);
+
+void drawBorders(int bx, int by){
+	int x=2, y=2;
+	for(x;x<=78;x++){
+		y=2;
+		gotoxy(x,y);printf("%c", bx);
+		y=22;
+		gotoxy(x,y);printf("%c", bx);
 	}
-	x = 2;
-	for(y;y<23;y++){
-		gotoxy(x,y);printf("%c",186);
-		gotoxy(x+76,y);printf("%c", 186);
+	for(y=2;y<=22;y++){
+		x=2;
+		gotoxy(x,y);printf("%c", by);
+		x=78;
+		gotoxy(x,y);printf("%c", by);
 	}
-	gotoxy(2,3);printf("%c",201);
-	gotoxy(2,23);printf("%c", 200);
-	gotoxy(78,3);printf("%c", 187);
-	gotoxy(78, 23);printf("%c", 188);
-	return;   
+	return;
 }
